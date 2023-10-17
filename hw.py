821898d814518a09b890fb520822ab1bfd89f26a
sys-rex <infant.rex@sysfore.com>
2023-10-17 05:23:29 +0000

    u1

--- hw.py
+++ hw.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, world HW py prod!!!"
+    return "Hello, world HW py uat!!!"
 
 if __name__ == "__main__":
  #   port = int(os.environ.get("PORT", 5010))
